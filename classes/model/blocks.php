5586fb0506dc049a603da779cfbcc21074efcabe
Thomas Edwards <tom@phpgenie.co.uk>
2012-06-15 12:50:33 +0100

    Added Model_Blocks

---
+++ classes/model/blocks.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Bromine CMS for FuelPHP
+ *
+ * @package    Bromine
+ * @version    1.0
+ * @author     Thomas Edwards <thomas@phpgenie.co.uk>
+ * @license    MIT License
+ * @copyright  2012 PHP Genie
+ * @link       http://bromine.phpgenie.co.uk
+ */
+
+namespace Bromine;
+
+/**
+ * Model_Blocks
+ */
+class Model_Blocks extends \Model_Crud
+{
+	protected static $_table_name = 'br_blocks';
+}
